03cb5081076f60d3e008b884c8d70e533d2742ec
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2026-08-30 17:49:31 +0300

    10. Patientor backend, step2

--- patientor/backend/index.ts
+++ patientor/backend/index.ts
@@ -1,8 +1,11 @@
 import express from "express"
+import cors from 'cors'
 
 const app = express()
 
-const PORT = 3000
+const PORT = 3001
+
+app.use(cors())
 
 app.get("/api/ping", (_req, res) => {
   console.log("someone pinged here")
