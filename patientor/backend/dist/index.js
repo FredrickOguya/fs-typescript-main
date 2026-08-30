6beeb75d4200e042e15b54f87fcfc090f071b34b
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2026-08-30 15:14:29 +0300

    9. Patientor backend, step1

---
+++ patientor/backend/dist/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+const app = express();
+const PORT = 3000;
+app.get("/api/ping", (_req, res) => {
+    console.log("someone pinged here");
+    res.send("pong");
+});
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+//# sourceMappingURL=index.js.map
---
+++ patientor/backend/index.ts
@@ -0,0 +1,14 @@
+import express from "express"
+
+const app = express()
+
+const PORT = 3000
+
+app.get("/api/ping", (_req, res) => {
+  console.log("someone pinged here")
+  res.send("pong")
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
