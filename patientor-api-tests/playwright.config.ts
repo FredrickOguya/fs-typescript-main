64891aa5d5d97f7959a1513e66df83ec3d6bead2
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2026-09-03 11:33:27 +0300

    start backend before api tests

--- patientor-api-tests/playwright.config.ts
+++ patientor-api-tests/playwright.config.ts
@@ -1,5 +1,11 @@
 import { defineConfig } from '@playwright/test';
 
+declare const process: {
+  env: {
+    CI?: string;
+  };
+};
+
 export default defineConfig({
   testDir: './tests',
   timeout: 30000,
@@ -8,8 +14,17 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
+
   use: {
     baseURL: 'http://localhost:3001',
     trace: 'on-first-retry',
+  },
+
+  webServer: {
+    command: 'npm run dev',
+    cwd: '../patientor/backend',
+    url: 'http://localhost:3001/api/ping',
+    timeout: 30000,
+    reuseExistingServer: !process.env.CI,
   },
 });
