20c93931c8416c8cb996329ba1b29f8ee9c76613
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2026-09-03 10:50:47 +0300

    16. Checkup

--- patientor/backend/src/index.ts
+++ patientor/backend/src/index.ts
@@ -1,7 +1,8 @@
-import express from "express";
+import express, { type NextFunction } from "express";
 import diagnosesRouter from './routes/diagnoses.ts';
 import patientsRouter from './routes/patients.ts';
 import cors from 'cors';
+import z from "zod";
 
 const app = express();
 app.use(express.json());
@@ -10,6 +11,19 @@ app.use(express.json());
 app.use(cors());
 app.use('/api/diagnoses', diagnosesRouter);
 app.use('/api/patients', patientsRouter);
+
+app.use((error: unknown, _req: express.Request, res: express.Response, next: NextFunction) => {
+  if (error instanceof z.ZodError) {
+    res.status(400).send({ error: error.issues});
+    return;
+  }
+  next(error);
+});
+
+app.get("/api/ping", (_req, res) => {
+  console.log("someone pinged here");
+  res.send("pong");
+});
 
 const PORT = 3001;
 
--- patientor/backend/src/types.ts
+++ patientor/backend/src/types.ts
@@ -19,7 +19,7 @@ export const NewPatientSchema = z.object({
   name: z.string(),
   dateOfBirth: z.iso.date(),
   ssn: z.string(),
-  gender: z.string(),
+  gender: z.enum(Gender),
   occupation: z.string()
 });
 
