86518d22690d5e479c57f58ea5935717cad15ab3
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2026-09-06 10:32:55 +0300

    24. Patientor, step 2

---
+++ patientor/frontend/src/components/PatientPage.tsx
@@ -0,0 +1,59 @@
+import {
+  Card,
+  CardContent,
+  Typography,
+  Stack,
+  Divider
+} from "@mui/material";
+
+import { Patient } from "../types";
+import { Female, Male } from "@mui/icons-material";
+
+interface Props {
+  patient: Patient | null | undefined;
+}
+
+const PatientPage = ({ patient }: Props) => {
+  if (!patient) {
+    return (
+      <Typography variant="h6">
+        Patient not found
+      </Typography>
+    );
+  }
+
+  return (
+    <Card sx={{ marginTop: 3 }}>
+      <CardContent>
+        <Typography variant="h4" gutterBottom>
+          {patient.name}  {patient.gender === "male" && <Male/> } {patient.gender === "female" && <Female />}
+        </Typography>
+
+        <Divider sx={{ marginBottom: 2 }} />
+
+        <Stack spacing={2}>
+          <Typography>
+            <strong>ssn: </strong> {patient.ssn}
+          </Typography>
+
+            
+          <Typography>
+            <strong>Occupation:</strong> {patient.occupation}
+          </Typography>
+
+          <Typography>
+            <strong>Date of Birth:</strong> {patient.dateOfBirth}
+          </Typography>
+          
+          {patient.gender === "other" &&
+          <Typography>
+            <strong>Gender:</strong> {patient.gender}
+          </Typography>
+          }
+        </Stack>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default PatientPage;
